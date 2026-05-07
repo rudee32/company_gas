1936ac5df3657ffa476de0c451780d7c6969056e
majid <150202384+rudee32@users.noreply.github.com>
2026-05-07 21:04:24 +0700

    Update About.jsx

--- src/components/landing/About.jsx
+++ src/components/landing/About.jsx
@@ -5,10 +5,10 @@ import { Shield, Truck, Award, Users, ChevronLeft, ChevronRight } from "lucide-r
 const companyImages = [
   "/images/perusahaan.png",
   "/images/perusahaan1.jpeg",
-  "/public/images/perusahaan2.jpeg",
-  "/public/images/perusahaan3.jpeg",
-  "/public/images/perusahaan4.jpeg",
-  "/public/images/perusahaan5.jpeg",
+  "/images/perusahaan2.jpeg",
+  "/images/perusahaan3.jpeg",
+  "/images/perusahaan4.jpeg",
+  "/images/perusahaan5.jpeg",
 ];
 
 const features = [
